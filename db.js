4d3bb382436febb6fad646669e1c60d830cc516d
lakshman2021 <lakshman.kinthali@tanla.com>
2024-03-30 00:56:53 +0530

    db changes

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'food_delivery',
-    password: 'test',
+    user: 'food_delivery_postgres',
+    host: 'dpg-co3ghv8l5elc73dd5esg-a',
+    database: 'food_delivery_s4a8',
+    password: 'AeHNN3sY4DR8AUVeGORUqKq8YXa3gV7b',
     port: 5432
 })
 
